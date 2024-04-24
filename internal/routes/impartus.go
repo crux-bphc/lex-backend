68219cb3bbb2145d9e22e86b81845ab132ea1200
TheComputerM <mudit.somani00@gmail.com>
2024-04-24 14:54:11 +0530

    feat: add basic register endpoint

--- internal/routes/impartus.go
+++ internal/routes/impartus.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"regexp"
 
 	"github.com/crux-bphc/lex/internal/impartus"
@@ -10,8 +11,42 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func registerUser(ctx *gin.Context) {
+	type requestBody struct {
+		Password string `json:"password" binding:"required"`
+		Token    string `json:"token" binding:"required"`
+	}
+	var body requestBody
+	ctx.BindJSON(&body)
+
+	// TODO: validate received impartus JWT
+
+	user := impartus.User{
+		// TODO: get email from keycloak
+		EMail:    "f2022xxxx@hyderabad.bits-pilani.ac.in",
+		Password: body.Password,
+		Jwt:      body.Token,
+	}
+
+	// create user in database
+	_, err := impartus.Repository.DB.Create("user", &user)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(200, gin.H{
+		"message": "Registered",
+		// TODO: add number of subjects/lectures added to database to response
+	})
+}
+
 func RegisterImpartusRoutes(router *gin.Engine) {
 	r := router.Group("/impartus")
+
+	r.POST("/register", registerUser)
 
 	// Returns the decryption key without the need for a Authorization header
 	r.GET("/lecture/:ttid/key", func(ctx *gin.Context) {
