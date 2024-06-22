54bd59cec63cc9c6242523c20ca499e15b23e5d1
TheComputerM <mudit.somani00@gmail.com>
2024-06-22 14:06:58 +0530

    feat: add stats endpoint

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/crux-bphc/lex/internal/routes"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/location"
 	"github.com/gin-gonic/gin"
+	stats "github.com/semihalev/gin-stats"
 )
 
 func main() {
 	router := gin.Default()
 	router.Use(cors.Default())
 	router.Use(location.Default())
+	router.Use(stats.RequestStats())
+
+	router.GET("/stats", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, stats.Report())
+	})
 
 	router.SetTrustedProxies(nil)
 
